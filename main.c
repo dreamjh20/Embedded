0d94335702f0a0592028cfd0af76f1abf54a5833
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-05-28 09:55:53 +0900

    Four fnd up&down with button

--- main.c
+++ main.c
@@ -1,25 +1,74 @@
-/*
- * Tuto_1.c
- *
- * Created: 2021-03-24 오후 10:31:55
- * Author : Moonvely_i
- */ 
-
+#define F_CPU 16000000
 #include <avr/io.h>
 #include <util/delay.h>
-
 int main(void)
 {
-	DDRA = 0xFF;
-	PORTA = 0x00;
-    /* Replace with your application code */
-    while (1)
-    {
-		PORTA = 0xFF;
-		_delay_ms(400);
-		PORTA = 0x00;
-		_delay_ms(400);
-    }
-	return 0;
-}
+	unsigned char fnd[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x90};
+	//unsigned char digit, fnd_position=0x01;
+	unsigned char sw=0;
+	int i=0, j=0, k=0, m=0;
+	DDRB=0xFF;
+	DDRD=0xFF;
+	DDRE = 0x00;
 
+	PORTB=0xFF;
+	PORTD=0xFF;
+	while(1)
+	{
+		if((PINE&0x01) == 0x00) sw=1;
+		else if((PINE&0x02) == 0x00) sw=2;
+
+		for(int h = 0; h < 2; h++){
+			PORTB=fnd[i];
+			PORTD=0x08; //왼쪽에서 첫번째 FND 지정
+			_delay_ms(1);
+			PORTB=fnd[j];
+			PORTD=0x04; //두번째 FND 지정
+			_delay_ms(1);
+			PORTB=fnd[k];
+			PORTD=0x02; //세번째 FND 지정
+			_delay_ms(1);
+			PORTB=fnd[m];
+			PORTD=0x01; //네번째 FND 지정
+			_delay_ms(1);
+		}
+		
+		if(sw == 1){
+			m++;
+			if(m > 9){
+				m=0;
+				k++;
+				if(k > 9){
+					k = 0;
+					j++;
+					if(j > 9){
+						j = 0;
+						i++;
+						if(i > 9){
+							i = 0;
+						}
+					}
+				}
+			}
+		}
+		
+		else if(sw == 2){
+			m--;
+			if(m < 0){
+				m=9;
+				k--;
+				if(k < 0){
+					k = 9;
+					j--;
+					if(j < 0){
+						j = 9;
+						i--;
+						if(i < 0){
+							i = 9;
+						}
+					}
+				}
+			}
+		}	
+	}
+}
