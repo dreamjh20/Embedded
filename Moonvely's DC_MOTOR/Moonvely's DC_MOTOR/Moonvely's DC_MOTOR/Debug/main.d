9956b7a3260980706ba79925ee26acd3a6f9d1a4
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-08-26 09:14:18 +0900

    FEAT: Speed UP Speed Down
    Control DC Motor and LED with UPDOWN Buttons

---
+++ Moonvely's DC_MOTOR/Moonvely's DC_MOTOR/Moonvely's DC_MOTOR/Debug/main.d
@@ -0,0 +1,46 @@
+main.d main.o: .././main.c \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Microchip\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom128.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Microchip\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom128.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Moonvely's DC_MOTOR/Moonvely's DC_MOTOR/Moonvely's DC_MOTOR/main.c
@@ -0,0 +1,109 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+volatile char speed=0;
+void DC_Motor(int speed)
+{
+	if(speed< 0) speed= 0;
+	if(speed>100) speed=100;
+	OCR1A=speed;
+}
+
+int main(void)
+{
+	int i = 0;
+	DDRA=0xFF;
+	DDRD=0x00;
+	DDRB=0x60;
+	TCCR1A=0x82;
+	TCCR1B=0x1A;
+	OCR1A=0;
+	OCR1B=0;
+	ICR1=100;
+	while(1)
+	{
+		if(PIND == 0x01)
+		{
+			if (i == 100)
+			{
+				i = 95;
+				PORTA=0x3E;
+			}
+			else if(i == 95)
+			{
+				i = 90;
+				PORTA=0x3C;
+			}
+			else if(i == 90)
+			{
+				i = 85;
+				PORTA=0x38;
+			}
+			else if(i == 85)
+			{
+				i = 80;
+				PORTA=0x30;
+			}
+			else if(i == 80)
+			{
+				i = 75;
+				PORTA=0x20;
+			}
+			else
+			{
+				i = 0;
+				PORTA=0x00;
+			}
+		}
+		else if(PIND == 0x02)
+		{
+			
+			if(i == 100)
+			{
+				i = 0;
+				PORTA=0x00;
+			}
+			else if(i == 0){
+				i = 75;
+				PORTA=0x20;
+			}
+			else if(i == 75){
+				i = 80;
+				PORTA=0x30;
+			}
+			else if(i == 80){
+				i = 85;
+				PORTA=0x38;
+			}
+			else if(i == 85){
+				i = 90;
+				PORTA=0x3C;
+			}
+			else if(i == 90){
+				i = 95;
+				PORTA=0x3E;
+			}
+			else if(i == 95){
+				i = 100;
+				PORTA=0x3F;
+			}
+		}
+		else if(PIND == 0x04)
+		{
+			PORTA=0x00;
+			i = 0;
+		}
+	
+		DC_Motor(i);
+		_delay_ms(500);
+		
+		/*
+		for(i=0;i<=100;i++) {
+			DC_Motor(i); _delay_ms(100);
+		}
+		for(i=100;i>=0;i--){
+			DC_Motor(i); _delay_ms(100);
+		}*/
+	}
+}
