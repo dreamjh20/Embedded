8de7831d0c327faf88c9ae9e605ac9ca8d7a9b86
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-10-05 11:50:25 +0900

    FEAT: 8LEDS

--- adc/adc/main.c
+++ adc/adc/main.c
@@ -1,11 +1,14 @@
 #include <avr/io.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
-#define CDS_10 341
-#define CDS_20 450
-#define CDS_50 550
-#define CDS_80 650
-#define CDS_100 731
+#define CDS_10 450
+#define CDS_20 485
+#define CDS_30 520
+#define CDS_40 555
+#define CDS_50 590
+#define CDS_60 625
+#define CDS_70 650
+#define CDS_100 685
 void int_adc();
 unsigned short read_adc();
 void show_adc_led(unsigned short data);
@@ -13,7 +16,7 @@ void show_adc_led(unsigned short data);
 int main(void)
 {
 	unsigned short value;
-	DDRA = 0x1F;
+	DDRA = 0xFF;
 	DDRF = 0x00;
 	init_adc();
 	while (1)
@@ -22,7 +25,6 @@ int main(void)
 		show_adc_led(value);
 	}
 }
-
 
 int init_adc(void)
 {
@@ -47,10 +49,13 @@ unsigned short read_adc()
 
 void show_adc_led(unsigned short value)
 {
-	if(value<=CDS_10) PORTA = 0x1F;
-	else if(value<=CDS_20) PORTA = 0x0F;
-	else if(value<=CDS_50) PORTA = 0x07;
-	else if(value<=CDS_80) PORTA = 0x03;
+	if(value<=CDS_10) PORTA = 0xFF;
+	else if(value<=CDS_20) PORTA = 0x7F;
+	else if(value<=CDS_30) PORTA = 0x3F;
+	else if(value<=CDS_40) PORTA = 0x1F;
+	else if(value<=CDS_50) PORTA = 0x0F;
+	else if(value<=CDS_60) PORTA = 0x07;
+	else if(value<=CDS_70) PORTA = 0x03;
 	else if(value<=CDS_100) PORTA = 0x01;
 	else PORTA = 0x00;
 }
