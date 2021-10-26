60da74fa8b94bfaa827013ff5869bf1e96dcc1a8
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-10-26 10:43:37 +0900

    CREATE: Laser Detect Object
    with cds

---
+++ Laser/Laser/Debug/main.d
@@ -0,0 +1,43 @@
+main.d main.o: .././main.c \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ D:\Utility\AtmelStudio\Dw\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom128a.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+D:\Utility\AtmelStudio\Dw\7.0\Packs\atmel\ATmega_DFP\1.6.364\include/avr/iom128a.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\utility\atmelstudio\dw\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Laser/Laser/main.c
@@ -0,0 +1,64 @@
+#include <avr/io.h>
+#define F_CPU 16000000UL
+#include <util/delay.h>
+
+#define CDS_10 450
+#define CDS_20 485
+#define CDS_30 520
+#define CDS_40 555
+#define CDS_50 590
+#define CDS_60 625
+#define CDS_70 650
+#define CDS_100 685
+
+void int_adc();
+unsigned short read_adc();
+void show_adc_led(unsigned short data);
+
+int main(void)
+{
+	unsigned short value;
+	
+	DDRA = 0xFF;
+	DDRF = 0x00;
+	DDRB = 0xFF;
+	init_adc();
+	
+	PORTA = 0x01;   //green on
+	_delay_ms(3000); //3초 대기
+	PORTB = 0x01;  // laser on
+	PORTA = 0x02;  //blue on
+	
+	while (1)
+	{
+		value = read_adc();
+		show_adc_led(value);
+	}
+}
+
+void init_adc(void)
+{
+	ADMUX = 0x40;
+	ADCSRA = 0x87;    // adc en
+}
+
+unsigned short read_adc()
+{
+	unsigned char adc_low, adc_high;
+	unsigned short value;
+
+	ADCSRA |= 0x40;    // ADC start
+	// ADC Complete
+	while((ADCSRA & 0x10) != 0x10);
+	adc_low = ADCL;
+	adc_high = ADCH;
+	value = (adc_high << 8) | adc_low;
+	
+	return value;
+}
+
+void show_adc_led(unsigned short value)
+{
+	if(value<=1000) PORTA = 0x04;     //object detected
+	else PORTA = 0x02;     //clear
+}
