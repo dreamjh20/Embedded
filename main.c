2a4bd60c08b553a28500cbd913d35b6701bd3567
dreamjh20 <68009161+dreamjh20@users.noreply.github.com>
2021-11-30 12:10:23 +0900

    CREATE: Smart Safe
    with laser, cds, leds, rgb leds, button, vr

--- main.c
+++ main.c
@@ -2,43 +2,77 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+int init_adc(void)
+{
+	ADMUX = 0x40;
+	ADCSRA = 0x87;    // adc en
+}
+
+unsigned short read_adc()
+{
+	unsigned char adc_low, adc_high;
+	unsigned short value;
+
+	ADCSRA |= 0x40;    // ADC start
+	// ADC Complete
+	while((ADCSRA & 0x10) != 0x10);
+	adc_low = ADCL;
+	adc_high = ADCH;
+	value = (adc_high << 8) | adc_low;
+	
+	return value;
+}
+
+void int_adc();
+unsigned short read_adc();
+void show_adc_led(unsigned short data);
+
+int status_led = 0;
+
 int main(void)
 {
-	int number = 0;
-    DDRD = 0x00;
+	int value;
+	
+	DDRF = 0x00;
 	DDRA = 0xff;
-	PORTA = 0xFF;
-	DDRE = 0x00;
+	DDRB = 0xff;
+	
+	DDRD = 0x00;
+	
+	DDRC = 0xff;
+	
+	init_adc();
+	
+	PORTB = 0X00;
+		
 
-    while (1) 
-	{
-		
-		if((PIND&0x10) == 0x00){
-			number++;
-			_delay_ms(100);
-		}
-		if((PINE&0x10) == 0x00){
-			number = 0;
-			_delay_ms(100);
+	while(1){
+		PORTA = 0x00;
+		PORTC = 0x00;
+		if((PIND&0x01) == 0x00){   //use button as a switch
+			status_led  = 1 - status_led;
+			    //delay to divide
 		}
 		
-		if(number == 0) PORTA = 0b11111111;
-		if(number == 1) PORTA = 0b11111110;
-		if(number == 2) PORTA = 0b11111101;
-		if(number == 3) PORTA = 0b11111100;
-		if(number == 4) PORTA = 0b11111011;
-		if(number == 5) PORTA = 0b11111010;
-		if(number == 6) PORTA = 0b11111001;
-		if(number == 7) PORTA = 0b11111000;
-		if(number == 8) PORTA = 0b11110111;
-		if(number == 9) PORTA = 0b11110110;
-		if(number == 10) PORTA = 0b11110101;
-		if(number == 11) PORTA = 0b11110100;
-		if(number == 12) PORTA = 0b11110011;
-		if(number == 13) PORTA = 0b11110010;
-		if(number == 14) PORTA = 0b11110001;
-		if(number == 15) PORTA = 0b11110000;
-		if(number > 15) PORTA = 0b11110000;
+		  
+		if(status_led == 1){   //locked
+			PORTB = 0x01;      //status led on
+			PORTA = 0x09;      //laser on
+			
+			if(value < 630){
+				PORTC = 0xff;
+			}
+			
+		}
+		else{                  //unlocked
+			PORTB = 0x00;      //status led off
+			PORTA = 0x04;      //laser off
+		}
 		
-    }
+		
+		_delay_ms(100);    
+	}
+	
 }
+
+
